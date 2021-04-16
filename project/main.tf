8360eff8f83275826366b06c3302aa52b9bb642a
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-04-16 16:07:53 +0200

    ♻ : refactor variable names

--- project/main.tf
+++ project/main.tf
@@ -1,20 +1,30 @@
+resource "random_id" "id" {
+  byte_length = 4
+  prefix      = var.project_name
+}
+
 # create the project itself
-resource "google_project" "challenge-lab" {
+resource "google_project" "this" {
   name            = var.project_name
-  project_id      = var.project_name
+  project_id      = random_id.id.hex
   org_id          = var.org_id
   billing_account = var.billing_account
 }
 
 # activate compute API on the project (GCS is enabled by default)
 resource "google_project_service" "project_services" {
-  project = google_project.challenge-lab.project_id
-  service = "compute.googleapis.com"
+  for_each = toset([ 
+      "compute.googleapis.com",
+      "logging.googleapis.com",
+    ])
+
+  project = google_project.this.project_id
+  service = each.key
 }
 
 # add myself as a owner of the project so i can view its content on the console
-resource "google_project_iam_member" "project" {
-  project = google_project.challenge-lab.project_id
+resource "google_project_iam_member" "me" {
+  project = google_project.this.project_id
   role    = "roles/owner"
   member  = "user:julien@codeka.io"
 }
--- project/service-account.tf
+++ project/service-account.tf
@@ -1,24 +1,24 @@
 # create a service account for the project
-resource "google_service_account" "terraform-sa" {
+resource "google_service_account" "terraform" {
   account_id = "terraform"
-  project    = google_project.challenge-lab.project_id
+  project    = google_project.this.project_id
 }
 
 # add some roles to the service account to be able to create a bucket
-resource "google_project_iam_member" "terraform-sa-roles" {
+resource "google_project_iam_member" "terraform-roles" {
   for_each = toset([ 
       "roles/compute.admin",
       "roles/storage.admin",
       "roles/iam.serviceAccountUser",
     ])
   
-  project = google_project.challenge-lab.project_id
-  member = "serviceAccount:${google_service_account.terraform-sa.email}"
+  project = google_project.this.project_id
+  member = "serviceAccount:${google_service_account.terraform.email}"
 
   role   = each.key
 }
 
 # create a key for this service account
-resource "google_service_account_key" "terraform-sa-key" {
-  service_account_id = google_service_account.terraform-sa.id
+resource "google_service_account_key" "terraform-key" {
+  service_account_id = google_service_account.terraform.id
 }
