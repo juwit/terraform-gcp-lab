13dbbf1cf0e4bfb337970a3479f4686f1ece3864
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-04-16 10:56:40 +0200

    🔒 : add service account

--- project.tf
+++ project.tf
@@ -18,3 +18,17 @@ resource "google_project_iam_member" "project" {
   role    = "roles/owner"
   member  = "user:julien@codeka.io"
 }
+
+# create a service account for the project
+resource "google_service_account" "terraform-sa" {
+  account_id = "terraform"
+  project    = google_project.challenge-lab.project_id
+}
+
+# add some roles to the service account to be able to create VM
+resource "google_project_iam_member" "terraform-sa" {
+  project = google_project.challenge-lab.project_id
+
+  role   = "roles/compute.admin"
+  member = "serviceAccount:${google_service_account.terraform-sa.email}"
+}
