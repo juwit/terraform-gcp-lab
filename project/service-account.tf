bbf02374967e8e4ed762badf9799a14aca56ff6d
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-04-19 15:25:26 +0200

    🎨 : terraform fmt

--- project/service-account.tf
+++ project/service-account.tf
@@ -6,16 +6,16 @@ resource "google_service_account" "terraform" {
 
 # add some roles to the service account to be able to create a bucket
 resource "google_project_iam_member" "terraform-roles" {
-  for_each = toset([ 
-      "roles/compute.admin",
-      "roles/storage.admin",
-      "roles/iam.serviceAccountUser",
-    ])
-  
-  project = google_project.this.project_id
-  member = "serviceAccount:${google_service_account.terraform.email}"
+  for_each = toset([
+    "roles/compute.admin",
+    "roles/storage.admin",
+    "roles/iam.serviceAccountUser",
+  ])
 
-  role   = each.key
+  project = google_project.this.project_id
+  member  = "serviceAccount:${google_service_account.terraform.email}"
+
+  role = each.key
 }
 
 # create a key for this service account
