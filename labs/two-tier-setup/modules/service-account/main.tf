8051f3d29137a19bce26dcb6830b137ec7085c7b
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-04-20 11:34:35 +0200

    🔒 : add roles/logging.logWriter role to service accounts

--- labs/two-tier-setup/modules/service-account/main.tf
+++ labs/two-tier-setup/modules/service-account/main.tf
@@ -7,3 +7,15 @@
 resource "google_service_account" "this" {
   account_id = var.service_account_name
 }
+
+resource "google_project_iam_binding" "roles" {
+  for_each = toset([
+    "roles/logging.logWriter"
+  ])
+
+  role    = each.key
+
+  members = [
+    "serviceAccount:${google_service_account.this.email}",
+  ]
+}
